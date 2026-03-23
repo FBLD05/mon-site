8a3a4d2fd7aa9cd7e158d70b42c2dad8f10a54c7
FBLD <tdrame289@gmail.com>
2026-03-23 09:30:46 +0000

    testttt

--- SALEMATA/script1.js
+++
@@ -1,8 +0,0 @@
-function order(produit) {
-  const numero = "221XXXXXXXXX"; // ton numéro
-  const message = "Bonjour, je veux commander : " + produit;
-
-  const url = "https://wa.me/" + numero + "?text=" + encodeURIComponent(message);
-
-  window.open(url, "_blank");
-}
